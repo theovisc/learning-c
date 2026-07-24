ed0952b7348202bc93110cc6b9f4c9ab34e82ff3
theovisc <viscomi.theo@gmail.com>
2026-07-24 12:47:27 +0200

    Day 1: First C program and Linux setup

---
+++ prinf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    // This is my first programm in C
+
+    /*
+    This 
+    is another commentary
+    */
+
+    printf("Hello world!!\n");
+    printf("I am alive\n");
+
+    return 0;
+}
